987c4cbc2a16bbc3bbd41c7eefc59840a7c68fea
batahualpa11 <brianatahualpa@gmail.com>
2025-05-29 23:36:44 -0400

    added lives system

--- recycle.js
+++ recycle.js
@@ -1,4 +1,3 @@
-
 //board
 let board;
 let boardWidth = 360;
@@ -19,6 +18,8 @@ let recyclingHeight = 30;
 let recyclingX = 30;
 let recyclingY = 0;
 
+//lives
+let lives = 3;
 
 //physics
 gravity = 2;
@@ -41,10 +42,15 @@ let basket = {
     y : basketY,
     width : basketWidth,
     height : basketHeight
-
 }
-window.onload = function() {
 
+//item types
+const itemTypes = [
+    { type: 'recyclable', color: 'green' },
+    { type: 'non-recyclable', color: 'red' }
+];
+
+window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
@@ -62,7 +68,6 @@ window.onload = function() {
 function update(){
     requestAnimationFrame(update);
     if(gameOver){
-
         return;
     }
     context.clearRect(0, 0, board.width, board.height);
@@ -80,31 +85,45 @@ function update(){
 
     //recycling
     for(let i = 0; i < recyclingArray.length; i++){
-        
         let recycle = recyclingArray[i];
         recycle.y += gravity;
-        context.fillStyle = colors[0];
-        context.fillRect(recycle.x,recycle.y,recycle.width,recycle.height);
+        context.fillStyle = recycle.color;
+        context.fillRect(recycle.x, recycle.y, recycle.width, recycle.height);
 
-        if(detectCollision(basket,recycle)){
-
-            score+= 1;
+        if(detectCollision(basket, recycle)){
+            if(recycle.type === 'recyclable') {
+                score += 1;
+            } else {
+                lives -= 1;
+                if(lives <= 0) {
+                    gameOver = true;
+                }
+            }
             recycle.x = 500;
         }
         if(recycle.x < boardWidth && recycle.y > boardHeight){
-
-            gameOver = true;
+            if(recycle.type === 'recyclable') {
+                lives -= 1;
+                if(lives <= 0) {
+                    gameOver = true;
+                }
+            }
         }
     }
     //clear caught recycling
     while(recyclingArray.length > 0 && recyclingArray[0].x == 500){
-
         recyclingArray.shift();
     }
+    
     //score 
     context.fillStyle = colors[1];
     context.font = "45px sans-serif";
     context.fillText(score, 5, 45);
+    
+    //lives
+    context.fillStyle = colors[3];
+    context.font = "30px sans-serif";
+    context.fillText("Lives: " + lives, 5, 80);
 
     if(score > speedUp){
         clearInterval(IntervalID);
@@ -114,72 +133,62 @@ function update(){
     }
 
     if(score > 100){
-
         gravity = 4;
     }
 
     if(gameOver){
         context.fillText("GAME OVER", boardWidth/8, boardHeight/2.2);
     }
-    
 }
 
 function placeRecycling(){
     if(gameOver){
-
         return;
     }
     let randomRecycleX = recyclingX + Math.random()*(boardWidth-60);
-
+    let randomType = Math.random() < 0.7 ? itemTypes[0] : itemTypes[1]; // 70% recyclable, 30% non-recyclable
 
     let recycling = {
-    x : randomRecycleX,
-    y : recyclingY,
-    width : recyclingWidth,
-    height : recyclingHeight,
-    collected : false
+        x: randomRecycleX,
+        y: recyclingY,
+        width: recyclingWidth,
+        height: recyclingHeight,
+        collected: false,
+        type: randomType.type,
+        color: randomType.color
     }
     
     recyclingArray.push(recycling);
 }
 
 function moveBasket(e){
-
     if(e.code == "ArrowLeft" || e.code == 'KeyA'){
-
         //move left
         basketMove = -3;
     }else if(e.code == "ArrowRight" || e.code == "KeyD"){
-
         //move right
         basketMove = 3;
     }else if(e.code == "Space"){
         if(gameOver){
-        basket.y = basketY;
-        basket.x = basketX;
-        score = 0;
-        recyclingArray = [];
-        speedUp = 10;
-        recycleInterval = 1500;
-        gameOver = false;
+            basket.y = basketY;
+            basket.x = basketX;
+            score = 0;
+            lives = 3;
+            recyclingArray = [];
+            speedUp = 10;
+            recycleInterval = 1500;
+            gameOver = false;
         }
     }
-
-
-
 }
 
 function stopBasket(e){
-
     basketMove = 0;
-
 }
 
 function detectCollision(a, b){
-
     return  a.x < b.x + b.width && 
             a.x + a.width > b.x && 
             a.y < b.y + b.height &&
             a.y + a.height > b.y;
-
 }
