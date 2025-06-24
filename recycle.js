504bbbc84998447e01fad3587c937a15a58b4bc1
Ajani King <ajaniking16@gmail.com>
2025-06-24 11:34:38 -0400

    added diff recyclable items/polishing

--- recycle.js
+++ recycle.js
@@ -19,9 +19,12 @@ let recyclingX = 30;
 let recyclingY = 0;
 
 //type of recycling
-let plastic = 1;
-let metal = 2;
-let paper = 3;
+let randomizedrecyclabletype = Math.floor(Math.random() * 3) + 1;
+const recytype = ["Plastic", "Metal", "Paper"];
+// plastic = 1 metal = 2 paper = 3
+const plastic = "blue";
+const metal = "grey";
+const paper = "brown";
 
 //lives
 let lives = 3;
@@ -37,10 +40,13 @@ let gameOver = false;
 
 //colors
 const colors = ['green', 'black', 'grey', 'red'];
+const Rcolors = [ 'blue', 'grey' , 'white'];
+
 
 //recycle place interval
 let recycleInterval = 1500;
 let speedUp = 10;
+let gravityUp = 20;
 
 let basket = {
     x : basketX,
@@ -51,11 +57,13 @@ let basket = {
 
 //item types
 const itemTypes = [
+    
     { type: 'recyclable', color: 'green' },
     { type: 'non-recyclable', color: 'red' }
 ];
 
 window.onload = function() {
+    
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
@@ -85,7 +93,7 @@ function update(){
     }else if(basket.x > boardWidth - basketWidth){
         basket.x = boardWidth - basketWidth;
     }
-    context.fillStyle = colors[2];
+    context.fillStyle = Rcolors[randomizedrecyclabletype-1];
     context.fillRect(basket.x, basket.y, basket.width, basket.height);
 
     //recycling
@@ -123,26 +131,42 @@ function update(){
     //score 
     context.textAlign = 'start';
     context.fillStyle = colors[1];
-    context.font = "45px sans-serif";
+    context.font = "30px sans-serif";
     context.fillText(score, 5, 45);
+
     
     //lives
     context.fillStyle = colors[3];
-    context.font = "30px sans-serif";
-    context.fillText("Lives: " + lives, 5, 80);
+    context.font = "20px sans-serif";
+    context.fillText("Lives : " + lives, 5, 80);
 
     //if recycle plastic, metal, or paper
+    context.fillStyle = Rcolors[randomizedrecyclabletype-1];
+    context.font = "30px sans-serif";
+    context.textAlign = "center"
+    context.fillText(recytype[randomizedrecyclabletype - 1], boardWidth / 2, 40);
+
+
 
     if(score > speedUp){
         clearInterval(IntervalID);
         recycleInterval += -50;
         speedUp += 10;
+         if(recycleInterval < 500){
+
+            recycleInterval = 500;
+            gravity += .5;
+        }
         IntervalID = setInterval(placeRecycling, recycleInterval);
+       
     }
 
-    if(score > 100){
-        gravity = 4;
+    if(score > gravityUp){
+        gravity += 1;
+        gravityUp += 10;
     }
+
+    
 
     if(gameOver){
     
@@ -151,18 +175,22 @@ function update(){
     context.fillRect(0, 0, boardWidth, boardHeight);
 
     // Gameover message
-    context.fillStyle = 'white';
+    context.fillStyle = 'red';
     context.font = "40px sans-serif";
     context.textAlign = 'center';
     context.fillText("GAME OVER", boardWidth / 2, boardHeight / 2 - 20);
 
+    context.font = "15px sans-serif";
+    context.fillText("You failed to recycle!", boardWidth / 2, boardHeight / 2 );
+    
     // Final Score
+    context.fillStyle = 'white';
     context.font = "30px sans-serif";
-    context.fillText("Score: " + score, boardWidth / 2, boardHeight / 2 + 30);
+    context.fillText("Score : " + score, boardWidth / 2, boardHeight / 2 + 45);
 
     // Restart
     context.font = "20px sans-serif";
-    context.fillText("Press SPACE to restart", boardWidth / 2, boardHeight / 2 + 70);
+    context.fillText("Press 'R' KEY to restart", boardWidth / 2, boardHeight / 2 + 80);
     
 }
     
@@ -174,8 +202,32 @@ function placeRecycling(){
     }
     let randomRecycleX = recyclingX + Math.random()*(boardWidth-60);
     let randomType = Math.random() < 0.7 ? itemTypes[0] : itemTypes[1]; // 70% recyclable, 30% non-recyclable
+    let recycling;
+    if(randomType.type === 'recyclable'){
+     
+    
+      let Rcolor;
+        if(randomizedrecyclabletype === 1){
+            Rcolor = 'blue';
+        }else if(randomizedrecyclabletype === 2){
+            Rcolor = 'grey';
+        }
+        else{
+            Rcolor ='white';
+        }
 
-    let recycling = {
+        recycling = {
+        x: randomRecycleX,
+        y: recyclingY,
+        width: recyclingWidth,
+        height: recyclingHeight,
+        collected: false,
+        type: randomType.type,
+        color: Rcolor
+        };
+    }      
+    else{
+        recycling = {
         x: randomRecycleX,
         y: recyclingY,
         width: recyclingWidth,
@@ -183,7 +235,11 @@ function placeRecycling(){
         collected: false,
         type: randomType.type,
         color: randomType.color
+        };
+
+        
     }
+    
     
     recyclingArray.push(recycling);
 }
@@ -195,8 +251,9 @@ function moveBasket(e){
     }else if(e.code == "ArrowRight" || e.code == "KeyD"){
         //move right
         basketMove = 3;
-    }else if(e.code == "Space"){
+    }else if(e.code == "KeyR"){
         if(gameOver){
+            randomizedrecyclabletype = Math.floor(Math.random() * 3) + 1;
             basket.y = basketY;
             basket.x = basketX;
             score = 0;
